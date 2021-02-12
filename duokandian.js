e651886c609791c6ba7803620abbff6425f0c8dc
xuxiaobo4510 <74219375+xuxiaobo4510@users.noreply.github.com>
2021-02-13 00:42:59 +0800

    Update duokandian.js

--- duokandian.js
+++ duokandian.js
@@ -74,7 +74,7 @@ duokandianheaderVal = {
 };
 if ($.isNode()) {
     // 没有设置 DKD_duokandianCASH 则默认为 0 不提现
-    CASH = process.env.DKD_duokandianCASH || 1;
+    CASH = process.env.DKD_duokandianCASH || 0.5;
 }
 if ($.isNode() && process.env.DKD_duokandianHEADER) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
