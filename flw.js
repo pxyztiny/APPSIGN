76fca6e54b48d252f05fe41018c30a895cb6bc29
xuxiaobo4510 <74219375+xuxiaobo4510@users.noreply.github.com>
2021-02-13 11:37:32 +0800

    Update flw.js

--- flw.js
+++ flw.js
@@ -94,9 +94,9 @@ let middleflwspBODY = [];
 let middleflwqwBODY = [];
 // 没有设置 FL_CASH 则默认为 0 不提现
 if ($.isNode()) {
-  CASH = process.env.FL_CASH || 0;
+  CASH = process.env.FL_CASH || 5;
   // 没有设置 FL_DHCASH 则默认为 0 不兑换
-  DHCASH = process.env.FL_DHCASH || 0;
+  DHCASH = process.env.FL_DHCASH || 5;
 }
 if ($.isNode() && process.env.FL_flwURL) {
   COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
