b159cfa27713b2418b303e333eaec3439f2f3dcb
xuxiaobo4510 <74219375+xuxiaobo4510@users.noreply.github.com>
2021-02-15 22:30:44 +0800

    Update zhimaCOOKIE.js

--- zhimaCOOKIE.js
+++ zhimaCOOKIE.js
@@ -2,7 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let zhimabodyVal= ``
+let zhimabodyVal= `appversion=10&desarr=9xeFa0AIVU061PHClHvK4dDt7uDXGMw9L0pv10XvzOrkDUGP11Md2jxca0FWPd2FcHQT9ieOsT2EQe%2BvUzANt6nDOyp5gwBGJEdVCWkqmrUhyR2SvoMS%2B6Tn%2BxfV5bfjPlKSqy0U9q0bhZYmuTXH8UvSYM0B6n7CKfXOVsLf9ErOVXXU4tYsbPoH6ks7lNohgqOjkTnn27OQ/zrD3sGoFULyB7BrkXzMi8%2B2jn4s5G8E%2BLbLDnSKX7UW14HOug7uxbOYgP2Dwm3iox8SKgzW%2B9pc3%2B%2Bjd95yJJ9w%2B6JTWl%2B0i6yz2tJaOOqfimXYxRBHZ2LuhZ/BRJKnoxhezeua7OAovcaCWzAIBfDsT630o8uq7lpx%2BTRk6A9e5eYr150chFLpaD9LwDd3v6%2Bx8jh7tdk4ziLfKyhuwkAtqrc/ppaCNZ3flub3bPZb2ID9g8Jt4qM6JfA7ma%2B6ZWft7jAvkunzziLfKyhuwkAwN1JM%2B7Im8TDUY3RrZ5zbtuCbVCw41jA `
 
 
 let zhimacookie = {
