0fa9bbf13aac81783e7c10ce230132313461146f
pxyztiny <77945661+pxyztiny@users.noreply.github.com>
2021-03-03 16:35:02 +0800

    Update bububaoCOOKIE.js

--- bububaoCOOKIE.js
+++ bububaoCOOKIE.js
@@ -4,9 +4,6 @@ let bububaotokenVal = ``
 
 
 
-let bububaocookie = {
-    bububaotokenVal: bububaotokenVal,
-
-}
+let bububaocookie = {"imei":"6e85025e-f87c-46b3-adea-5cde20865bac","ini":"29","version":"18","tokenstr":"B4438A6B3A2EDD6688E1A73C1532500G1614653034","store":"0","platform":"1","Content-Type":"application/x-www-form-urlencoded","User-Agent":"Dalvik/2.1.0 (Linux; U; Android 10; GLK-AL00 Build/HUAWEIGLK-AL00)","Host":"bububao.duoshoutuan.com","Connection":"Keep-Alive","Accept-Encoding":"gzip","Content-Length":"0"}
 
 module.exports = bububaocookie
