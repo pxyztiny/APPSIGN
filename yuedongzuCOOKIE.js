eeb7ea3eb98454f3245ac70a7dedfca9f89cb49d
pxyztiny <77945661+pxyztiny@users.noreply.github.com>
2021-03-03 14:41:20 +0800

    Add files via upload

---
+++ yuedongzuCOOKIE.js
@@ -0,0 +1,22 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let yuedongzutokenVal = "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wveXVlZG9uZ3p1LnlpY2hlbmd3LmNuXC9hcHBzXC9sb2dpblwvd2VjaGF0IiwiaWF0IjoxNjE0NjcwMDA1LCJleHAiOjIwNTM2MjcwMDA1LCJuYmYiOjE2MTQ2NzAwMDUsImp0aSI6IlhjVmFtZlZEUnhxNlFvT1oiLCJzdWIiOjMxNzk2LCJwcnYiOiI0MWRmODgzNGYxYjk4ZjcwZWZhNjBhYWVkZWY0MjM0MTM3MDA2OTBjIn0.-XKB8vOBi2j6u40rzYT3cwE2P3Av6UiPKP5s2eGOCxU"
+
+
+
+let yuedongzucookie = {
+    "imei": "6e85025e-f87c-46b3-adea-5cde20865bac",
+    "store": "3",
+    "ini": "29",
+    "version": "5",
+    "platform": "1",
+    "Authorization": "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wveXVlZG9uZ3p1LnlpY2hlbmd3LmNuXC9hcHBzXC9sb2dpblwvd2VjaGF0IiwiaWF0IjoxNjE0NjcwMDA1LCJleHAiOjIwNTM2MjcwMDA1LCJuYmYiOjE2MTQ2NzAwMDUsImp0aSI6IlhjVmFtZlZEUnhxNlFvT1oiLCJzdWIiOjMxNzk2LCJwcnYiOiI0MWRmODgzNGYxYjk4ZjcwZWZhNjBhYWVkZWY0MjM0MTM3MDA2OTBjIn0.-XKB8vOBi2j6u40rzYT3cwE2P3Av6UiPKP5s2eGOCxU",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 10; GLK-AL00 Build/HUAWEIGLK-AL00)",
+    "Host": "yuedongzu.yichengw.cn",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "Content-Length": "0"
+  }
+
+module.exports = yuedongzucookie
